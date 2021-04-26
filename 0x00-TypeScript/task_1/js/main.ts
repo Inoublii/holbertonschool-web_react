7834a2ce0e3652f1b0faa2246cad3a037097aad9
inoublii <inoublii@gmail.com>
2021-04-26 19:10:57 +0100

    TypeScript

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -6,13 +6,15 @@ interface Teacher {
 	location: string;
 	[propName: string]: any;
   }
+
 interface Directors extends Teacher {
 numberOfReports: number;
 }
+
 interface printTeacherFunction {
 	(firstName: string, lastName: string): string,
-  }
+}
 
 const printTeacher: printTeacherFunction = (firstName, lastName) => {
-	return `${firstName[0]}. ${lastName}`;
-  };
+  return `${firstName[0]}. ${lastName}`;
+};
