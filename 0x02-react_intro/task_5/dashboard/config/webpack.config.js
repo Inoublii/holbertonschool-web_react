3e30c1f5e49cc3809c757d46ed8b27a60a9b09f8
inoublii <inoublii@gmail.com>
2021-04-28 23:44:33 +0100

    task8

---
+++ 0x02-react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+	devtool: 'inline-source-map',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+	devServer: {
+		hot: true,
+		contentBase: path.resolve(__dirname, 'dist'),
+	},
+	plugins: [
+		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({
+			template: './dist/index.html',
+      title: 'Holberton Dashboard',
+    }),
+	],
+	module: {
+		rules: [
+
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource',
+				loader: 'image-webpack-loader',
+			},
+		],
+	},
+};
