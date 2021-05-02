d6898689c14eb608ba19715d050eca189c761278
inoublii <inoublii@gmail.com>
2021-05-02 19:38:15 +0200

    Task8

--- 0x04-React_component/task_4/dashboard/src/HOC/WithLogging.js
+++ 0x04-React_component/task_4/dashboard/src/HOC/WithLogging.js
@@ -1,18 +1,18 @@
 import React, { Component, Children } from 'react';
-export default class WithLogging extends Component{
-	constructor(props) {
-        super(props);  
+
+export default class WithLogging extends Component {
+	constructor(props) {super(props);};
+
+	componentDidMount() { let comps = this.props.children.type.name || 'Component';
+		console.log(`Component ${comps} is mounted`);
 	};
-    
-    componentDidMount() {
-		let comp = this.props.children.type.name || 'Component';
-		console.log(`Component ${comp} is mounted`);
-      };
-      componentWillUnmount() {
-		let comp = this.props.children.type.name || 'Component';
-        console.log(`Component ${comp} is going to unmount`);
-      };
-      render() {
-        return (this.props.Children);
-      };
-    };
+
+	componentWillUnmount() { let comps = this.props.children.type.name || 'Component';
+		console.log(`Component ${comps} is going to unmount`);
+	};
+
+	render() {
+		return (this.props.children);
+	};
+};
+
