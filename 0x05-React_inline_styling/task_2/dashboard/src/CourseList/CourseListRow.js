f070ab9c2721251ad63a52505d7385ba63779cc1
inoublii <inoublii@gmail.com>
2021-05-04 02:37:19 +0100

    Tas2

--- 0x05-React_inline_styling/task_2/dashboard/src/CourseList/CourseListRow.js
+++ 0x05-React_inline_styling/task_2/dashboard/src/CourseList/CourseListRow.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types'; // ES6
+import { StyleSheet, css } from 'aphrodite';
 
 export default function CourseListRow({ isHeader, textFirstCell, textSecondCell }) {
   let trContent = '';
@@ -10,7 +11,7 @@ export default function CourseListRow({ isHeader, textFirstCell, textSecondCell 
 
   if (isHeader) {
     if (textSecondCell === null) {
-      trContent = (<th colSpan='2'>{textFirstCell}</th>);
+		trContent = (<th colSpan='2' className={css(style.thFirt, style.thTd)}>{textFirstCell}</th>);
     }
     else {
       trContent = (<React.Fragment><th>{textFirstCell}</th><th>{textSecondCell}</th></React.Fragment>);
@@ -18,8 +19,17 @@ export default function CourseListRow({ isHeader, textFirstCell, textSecondCell 
   } else {
     trContent = (<React.Fragment><td>{textFirstCell}</td><td>{textSecondCell}</td></React.Fragment>);
   }
-  return (<tr style={style}>{trContent}</tr>);
+  return (<tr style={styleInLine}>{trContent}</tr>);
 }
+const style = StyleSheet.create({
+	thFirt: {
+	  textAlign: 'center',
+	},
+	thTd: {
+	  padding: '0.25rem',
+	  border: '1px solid lightgray',
+	}
+  });
 
 CourseListRow.propTypes = {
   isHeader: PropTypes.bool,
