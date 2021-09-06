8062628d80568f85e370d985f7465b90e778d674
inoublii <inoublii@gmail.com>
2021-09-06 22:50:18 +0100

    fixed

--- 0x08_react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
+++ 0x08_react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
@@ -4,3 +4,9 @@ export const getAllNotificationsByUser = (userId) => {
   return data.default.filter((item) => item.author.id === userId)
   .map(({ context }) => context);
 };
+
+const user = new schema.Entity('users');
+const message = new schema.Entity('messages', {}, { idAttribute: 'guid' });
+const notification = new schema.Entity('notifications', { author: user, context: message });
+
+export const normalizedData = normalize(data, [notification]);
