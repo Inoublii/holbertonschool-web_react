e9da43938f5abd4565ea492874a40847d1a6babb
inoublii <inoublii@gmail.com>
2021-09-06 15:51:33 +0100

    fix test case

--- 0x06-react_state/task_0/dashboard/src/Notifications/NotificationItem.test.js
+++ 0x06-react_state/task_0/dashboard/src/Notifications/NotificationItem.test.js
@@ -9,6 +9,8 @@ configure({adapter: new Adapter()});
 
 describe("Testing <NotificationItem /> Component", () => {
 
+	let wrapper;
+
 	beforeEach(() => {
 		StyleSheetTestUtils.suppressStyleInjection();
 	});
@@ -17,8 +19,6 @@ describe("Testing <NotificationItem /> Component", () => {
 		StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
 	});
 
-	let wrapper;
-
 	it("<NotificationItem /> is rendered without crashing", () => {
 		wrapper = shallow(<NotificationItem shouldRender />);
 
@@ -26,61 +26,38 @@ describe("Testing <NotificationItem /> Component", () => {
 		expect(wrapper).to.not.be.an("undefined");
 	});
 
-
-
-	it("<NotificationItem />  passing value props", () => {
-
+	it("<NotificationItem /> render the correct HTML, by passing type and value props", () => {
 		let props = {
 			type: "default",
 			value: "New resume",
 			html: undefined
 		}
-
-		let component = shallow(<NotificationItem {...props} />);
-
-		console.log(component);
-		expect(component.contains(< li data-priority-type={props.type} dangerouslySetInnerHTML={undefined}>New resume </li>)).to.equal(false);
+		let component = shallow(<NotificationItem {...props} shouldRender />);
+		expect(component.containsAllMatchingElements([
+			<li data-priority-type={props.type}>{props.value}</li>
+		])).to.equal(true);
 	});
-	it("spy as the markAsRead property and Check that when simulating a click on the component, the spy is called with the right ID argument", () => {
+
+	it("<NotificationItem /> render the correct HTML, by passing dummy html props", () => {
+		let props = {
+			type: "urgent",
+			html: { __html: "<p>test</p>"},
+		}
+		let component = shallow(<NotificationItem {...props} />);
+		expect(component.containsAllMatchingElements([
+			<li data-priority-type={props.type} dangerouslySetInnerHTML={props.html} />,
+		])).to.equal(true);
+	});
+
+	it("Verify that when Clicking on the component, the 'markAsRead' is called with the right ID argument", () => {
 		let props = {
 			type: "urgent",
 			html: { __html: "<p>test</p>"},
 			markAsRead: (id) => { console.log(`Notification ${id} has been marked as read`)}
 		};
-	it("Verify that clicking on the menu item calls handleDisplayDrawer", () => {
-		const mockDisplay = jest.fn(() => {});
-		const mockHide = jest.fn(() => {});
-
-		let props = {
-			...props2,
-			displayDrawer: false,
-			handleDisplayDrawer: mockDisplay,
-			handleHideDrawer: mockHide,
-		};
-
-		let wrapper = shallow(<Notifications {...props} />);
-
-		wrapper.find('.menuItem').at(0).simulate('click');
-
-		expect(mockDisplay.mock.calls.length).to.equal(1);
-	});
-
-	it("Verify that clicking on the button calls handleHideDrawer", () => {
-		const mockDisplay = jest.fn(() => {});
-		const mockHide = jest.fn(() => {});
-
-		let props = {
-			...props2,
-			displayDrawer: true,
-			handleDisplayDrawer: mockDisplay,
-			handleHideDrawer: mockHide,
-		};
-
-		wrapper = shallow(<NotificationItem {...props} />);
-		console.log = jest.fn();
+gi		console.log = jest.fn();
 		wrapper.find('li').simulate('click');
 		expect(console.log.mock.calls.length).to.equal(1);
-
 	});
 
 });
