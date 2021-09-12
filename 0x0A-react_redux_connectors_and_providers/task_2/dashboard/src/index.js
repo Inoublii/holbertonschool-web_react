bf797e81f7749c23f32431e0c0a87dcb3407b483
inoublii <inoublii@gmail.com>
2021-09-12 16:39:34 +0100

    applyMiddleware

--- 0x0A-react_redux_connectors_and_providers/task_2/dashboard/src/index.js
+++ 0x0A-react_redux_connectors_and_providers/task_2/dashboard/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App/App';
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import uiReducer, { initialState } from "./reducers/uiReducer";
 import { Map } from "immutable";
 
 
-const store = createStore(uiReducer, Map(initialState));
+const store = createStore(uiReducer, Map(initialState), applyMiddleware(thunk));
 
 ReactDOM.render(
 <React.StrictMode>
